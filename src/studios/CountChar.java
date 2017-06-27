155aeb0aa3d334313a3b52c44a5cf6003da4872e
AnnaHey <atekton99@gmail.com>
2017-06-27 14:31:45 -0500

    began char count

--- src/studios/CountChar.java
+++ src/studios/CountChar.java
@@ -4,4 +4,8 @@ package studios;
  * Created by Anna Hexter on 6/27/2017.
  */
 public class CountChar {
+    public static void main(String[] args) {
+        String testString = "Lorem ipsum:";
+
+    }
 }
